46aa62a523d23ecafd3fdad698f283db9abad0c3
mpaturu <mpaturu@gmail.com>
2026-07-25 23:02:02 -0400

    Clarify freshShopRegVerification function call
    Added comment to clarify the purpose of freshShopRegVerification call.

--- assets/js/sap-cdc.js
+++ assets/js/sap-cdc.js
@@ -362,7 +362,7 @@
                 onAfterSubmit: function (e) {
                     if (e.screen === 'mpaturu-gigya-register-screen' && e.response.errorCode === 206002) {
                         showToast("Your profile has been successfully created.");
-                        freshShopRegVerification(e.response.UID);
+                        freshShopRegVerification(e.response.UID); // Call the function to get JWT and call SAP API
                         gigya.accounts.showScreenSet({
                             screenSet: 'mpaturu-RegistrationLogin',
                             startScreen: 'mpaturu-gigya-login-screen',
