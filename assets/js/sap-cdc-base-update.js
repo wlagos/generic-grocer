69598db2634884c75745fbfa82d03143f0ddcad7
mpaturu <mpaturu@gmail.com>
2026-09-08 21:32:53 -0400

    Update sap-cdc-base-update.js

--- assets/js/sap-cdc-base-update.js
+++ assets/js/sap-cdc-base-update.js
@@ -24,7 +24,6 @@
   },
   onBeforeScreenLoad: function (event) {
     var doc = document;
-    doc.getElementById('gigya-textbox-112545187415970690').value = "";
     if (!doc.__cdcNs) {
       doc.__cdcNs = {
         helpers: {
